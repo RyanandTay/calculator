ac0311a61021c3de07793fb66ada6f8a79003753
RyanandTay <202003716+RyanandTay@users.noreply.github.com>
2025-09-16 10:52:44 -0600

    Implement calculation execution logic
    - Add handling for special functions such as exponent and square root
    - Support chained operations by executing when a new operator is pressed
    - Add logic to run calculatorFunctions based on selected operator

--- script.js
+++ script.js
@@ -1,147 +1,215 @@
 
 
 const calculatorFunctions = {
-    addition(firstNumber, secondNumber) {
+    // Addition
+    '+'(firstNumber, secondNumber) {
         return +firstNumber + +secondNumber;
     },
 
-    subtraction(firstNumber, secondNumber) {
+    // Subtraction
+    '-'(firstNumber, secondNumber) {
         return +firstNumber - +secondNumber;
     },
 
-    division(firstNumber, secondNumber) {
-        return +firstNumber / +secondNumber;
+    // Division
+    '/'(firstNumber, secondNumber) {
+        if (secondNumber === "0") {
+            return "Oops! Div 0"
+        } else {
+            return +firstNumber / +secondNumber;
+        }
     },
 
-    multiplication(firstNumber, secondNumber) {
+    // Multiplication
+    '*'(firstNumber, secondNumber) {
         return +firstNumber * +secondNumber;
     },
 
-    exponent(firstNumber, secondNumber) {
+    // Exponent
+    "xy"(firstNumber, secondNumber) {
         return Math.pow(+firstNumber, +secondNumber);
     },
 
-    squareRoot(firstNumber) {
+    // Square root
+    "√X"(firstNumber) {
         return Math.sqrt(+firstNumber);
     },
 
-    percentage(firstNumber) {
+    // Percentage
+    "%"(firstNumber) {
         return +firstNumber / 100;
     },
 
-    roundZero(firstNumber) {
+    // Round Zero
+    "R0"(firstNumber) {
         return Math.round(+firstNumber);
     },
-        
-    roundTwo(firstNumber) {
-        return parseFloat(+firstNumber.toFixed(2));
+    
+    // Round Two
+    "R2"(firstNumber) {
+        return parseFloat(Number(firstNumber).toFixed(2));
     },
 };
 
-const inputText = document.querySelector("#calculatorText");
+let currentNumber ="";
 let firstNumber ="0";
-const numberButtons = [...document.querySelectorAll(".numberButton")];
-numberButtons.forEach(button => {
+let operator ="";
+let secondNumber ="";
+
+const allowedInputArray = 
+['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    '.', '+', '-', '/', '*', '=',
+    "%", "√X", "π", "R0", "R2", "xy",
+    "Backspace", "Escape", "Enter"
+];
+
+const inputText = document.querySelector("#calculatorText");
+inputText.value = "0";
+inputText.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    getInput(event.key);    
+});
+
+const calculatorContainer = document.querySelector(".calculatorButtons");
+let calculatorButtons = [...calculatorContainer.querySelectorAll("button")];
+
+calculatorButtons = calculatorButtons.filter(button => button.textContent !== "CA");
+
+calculatorButtons.forEach(button => {
     button.addEventListener("click", () => {
-        const clickedButton = button.textContent;
-        handleInput(clickedButton);
+        let input = button.textContent;
+        if (input === "x") {
+            input = "*";
+        } else if (input === "÷") {
+            input = ("/");
+        }
+        getInput(input);
     });
 });
 
-inputText.value = "0";
-
-// will be a function called getFirstNumber
-inputText.addEventListener("keydown", (event) => {
-
+function getInput(input) {
     switch (true) {
-        case !(allowedInputArray.includes(event.key)):
-            event.preventDefault();
+        case !(allowedInputArray.includes(input)):
             return;
 
-        case event.key === ".":
-            event.preventDefault();
+        case input === ".":
             handleDecimalInput();
             break;
 
-        case event.key === "-":
-            event.preventDefault();
-            handleMinusInput();
-            break;
-
-        case !(isNaN(event.key)):
-            event.preventDefault();
-            handleNumberInput(event.key);
+        case !(isNaN(input)):
+            handleNumberInput(input);
             break;
         
-        case event.key === "Backspace":
-            event.preventDefault();
+        case input === "Backspace":
             handleBackspaceInput();
             break;
 
-        default: 
-            getOperator(event.key);
+        case (input === "Enter" || input === "="):
+            operate();
+            break;
+
+        case input === "-" && currentNumber === "":
+            currentNumber = "-";
+            inputText.value = currentNumber;
+            break;
+
+        case input === "π":
+            input = "3.14159";
+            inputText.value = currentNumber;
+            handleNumberInput(input);
+            break;
+        
+        case input === "%":
+        case input === "R0":
+        case input === "R2":
+        case input === "√X":
+            operator = input;
+            secondNumber = "0";
+            operate();
+            break;
+
+        default:
+            if (secondNumber !== "") {
+                operate();
+            }
+            operator = input;
+            secondNumber = "";
+            currentNumber = "";
+            inputText.value = firstNumber;
             break;
         }
-    }
-);
-
-function handleDecimalInput() {
-    if (!(firstNumber.includes("."))) {
-        if (firstNumber === "") {
-            firstNumber += "0."
-            inputText.value = firstNumber;
-        } else {
-            firstNumber += ".";
-            const [integerPart, decimalPart] = firstNumber.split(".");
-            const formattedInteger = Number(integerPart).toLocaleString();
-            inputText.value = `${formattedInteger}.${decimalPart}`;
-        }
-    } else return;
 }
 
-function handleMinusInput() {
-    if (firstNumber === "0" || firstNumber === "") {
-        firstNumber = "-";
-        inputText.value = firstNumber;
+function handleDecimalInput() {
+    if (!(currentNumber.includes("."))) {
+        if (currentNumber === "") {
+            currentNumber += "0."
+            inputText.value = currentNumber;
+        } else {
+            currentNumber += ".";
+            formatDecimalNumber();
+        }
     } else {
-        operate();
+        formatDecimalNumber();
     }
+}
+
+function formatDecimalNumber() {
+    const [integerPart, decimalPart] = currentNumber.split(".");
+    const formattedInteger = Number(integerPart).toLocaleString();
+    inputText.value = `${formattedInteger}.${decimalPart}`;
 }
 
 function handleNumberInput(inputNumber) {
-    firstNumber += inputNumber;
-
-    if ((firstNumber.includes("."))) {
-        const [integerPart, decimalPart] = firstNumber.split(".");
-        const formattedInteger = Number(integerPart).toLocaleString();
-        inputText.value = `${formattedInteger}.${decimalPart}`;
+    currentNumber += inputNumber;
+    if (operator === "") {
+        firstNumber = currentNumber;
     } else {
-        inputText.value = Number(firstNumber).toLocaleString();
+        secondNumber = currentNumber;
+    }
+
+    if ((currentNumber.includes("."))) {
+        formatDecimalNumber(currentNumber);
+    } else {
+        inputText.value = Number(currentNumber).toLocaleString();
     }  
 }
 
 function handleBackspaceInput() {
-    if (firstNumber.length > 0) {
-        firstNumber = firstNumber.slice(0, -1);
+    if (currentNumber.length > 0) {
+        currentNumber = currentNumber.slice(0, -1);
     }
 
-    if (firstNumber === "" || firstNumber === "-") {
+    if (currentNumber === "" || currentNumber === "-") {
         inputText.value = "0";
-        firstNumber = "";
+        currentNumber = "";
     } else {
-        if (firstNumber.includes(".")) {
-            const [integerPart, decimalPart] = firstNumber.split(".");
-            const formattedInteger = Number(integerPart).toLocaleString();
-            inputText.value = `${formattedInteger}.${decimalPart}`;
+        if (currentNumber.includes(".")) {
+            formatDecimalNumber(currentNumber);
         } else {
-            inputText.value = Number(firstNumber).toLocaleString();
+            inputText.value = Number(currentNumber).toLocaleString();
         }
     }
 }
 
-function clearAll() {
-    firstNumber ="0";
-    inputText.value ="0";
+function operate() {
+    let operationResult = ""
+    if (secondNumber === "") {
+        return;
+    } else {
+        operationResult = calculatorFunctions[operator](firstNumber, secondNumber);
+    }
+
+    firstNumber = String(operationResult);
+
+    if (typeof operationResult === "string") {
+        inputText.value = firstNumber;
+        currentNumber = "";
+        firstNumber = "";
+        operator = "";
+    } else {
+        inputText.value = Number(firstNumber).toLocaleString();
+    }
 }
 
 const clearAllButton = document.querySelector(".clearEntryOrAll");
@@ -150,8 +218,10 @@ document.addEventListener("keydown", (event) => {
     if (event.key === "Escape") clearAll();
 });
 
-const allowedInputArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    '.', '+', '-', '/', '*',
-    "Backspace", "Enter", "Escape", "ArrowLeft", "ArrowRight"
-];
-
+function clearAll() {
+    currentNumber ="";
+    firstNumber = "0";
+    operator = "";
+    secondNumber = "";
+    inputText.value = "0";
+}
